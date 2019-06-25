5d33397b7a903cac129197555aac4bb932082e55
Alouiscious Sims <info@reneesoul.com>
2019-06-25 08:30:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,17 +27,22 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-
+  driftBoating = ["PFD", "trolling motor", "rod", "bait"]
+  driftBoating.shift("PFD")
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  driftBoating = ["PFD", "trolling motor", "rod", "bait"]
+  driftBoating.[2]
 end
 
 def retrieve_first_element_from_array(array)
+  driftBoating = ["PFD", "trolling motor", "rod", "bait"]
+  driftBoating.[0]
 
 end
 
 def retrieve_last_element_from_array(array)
-
+  driftBoating = ["PFD", "trolling motor", "rod", "bait"]
+  driftBoating.[-1]
 end
